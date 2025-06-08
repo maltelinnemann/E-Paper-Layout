448ae055a7214b38737575ef5d130d354382b8cb
Malte Linnemann <M.Linnemann@SLX.DE>
2025-06-08 17:08:16 +0200

    Update font sizes in layoutGenerator for improved readability

--- layoutGenerator.py
+++ layoutGenerator.py
@@ -18,9 +18,9 @@ def generate_layout():
     draw_red = ImageDraw.Draw(img_red)
 
     # Schriftarten laden
-    font_large = ImageFont.truetype("fonts/DejaVuSans-Bold.ttf", 46)
-    font_medium = ImageFont.truetype("fonts/DejaVuSans.ttf", 36)
-    font_small = ImageFont.truetype("fonts/DejaVuSans.ttf", 30)
+    font_large = ImageFont.truetype("fonts/DejaVuSans-Bold.ttf", 48)
+    font_medium = ImageFont.truetype("fonts/DejaVuSans.ttf", 40)
+    font_small = ImageFont.truetype("fonts/DejaVuSans.ttf", 34)
 
     # Icons laden 
     sunIcon = Image.open("icons/sun.png").convert("1").resize((70, 70), Image.LANCZOS)
